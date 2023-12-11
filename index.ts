640bd606b513b3ca5176b63aebc8d7911ec0d5d6
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-12-11 22:01:18 +0900

    html作成

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(3000, () => {
